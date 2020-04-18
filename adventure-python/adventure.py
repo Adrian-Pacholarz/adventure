92098da70e468131f5d0c56988a2be9eaada62cf
Adrian <flojd_xl@wp.pl>
2020-04-18 13:52:07 +0200

    Implemented movement

--- adventure-python/adventure.py
+++ adventure-python/adventure.py
@@ -1,4 +1,4 @@
-import termcolor
+#import termcolor
 
 player = {
     'room': 'outside',
@@ -8,12 +8,38 @@ rooms = {
     "outside": {
         "title": "Outside",
         "description": "You are standing outside of a huge cave entrance.",
-        "movemnets": "norht"
+        "north": "cave",
+        "movements": "There are the following exits: north"
     },
     "cave": {
         "title": "Cave",
         "description": "You're in a cave.",
+        "south": "outside",
+        "north": "room1",
+        "movements": "There are the following exits: south, north"
     },
+    "room1": {
+        "title": "Room One",
+        "description": "You're in a huge room inside the cave",
+        "south": "cave",
+        "east": "tunnel",
+        "west": "catacombs",
+        "movements": "There are the following exits: south, east, west"
+    },
+    "catacombs": {
+        "title": "Entrance to catacombs",
+        "description": "You're in an entrace to catacombs with many options to go further",
+        "east": "room1",
+        "west": "cave", # it goes back there
+        "movements": "There are the following exits: east, west"
+    },
+    "tunnel": {
+        "title": "Tunnel",
+        "description": "You're in a small passage",
+        "west": "cave",
+        "north": "tunnel",
+        "movements": "There are the following exits: west, north"
+    }
 }
 
 
@@ -21,22 +47,29 @@ def main():
     describe_room()
     playing = True
     while playing:
-        command = get_command()
-        if command in ['look', 'l']:
-            describe_room()
-        elif command in ['quit', 'q']:
-            print('Bye!')
-            playing = False
-        elif command in ['north',  'n']:
-            pass
-        elif command in ['south', 's']:
-            pass
-        elif command in ['east', 'e']:
-            pass
-        elif command in [ 'west', 'w']:
-            pass
-        else:
-            print(f'Unrecognized command: {command}')
+        command = get_command().lower()
+        try:
+            if command in ['look', 'l']:
+                describe_room()
+            elif command in ['quit', 'q']:
+                print('Bye!')
+                playing = False
+            elif command in ['north',  'n']:
+                player['room'] = rooms[player['room']]["north"]
+                describe_room()
+            elif command in ['south', 's']:
+                player['room'] = rooms[player['room']]["south"]
+                describe_room()
+            elif command in ['east', 'e']:
+                player['room'] = rooms[player['room']]["east"]
+                describe_room()
+            elif command in [ 'west', 'w']:
+                player['room'] = rooms[player['room']]["west"]
+                describe_room()
+            else:
+                print(f'Unrecognized command: {command}')
+        except KeyError:
+            print(f'There is nothing there, current location is still: {player["room"]}')
 
 
 def get_command():
@@ -50,6 +83,7 @@ def describe_room():
     print(room['title'])
     print()
     print(room['description'])
+    print(room["movements"])
 
 
 if __name__ == '__main__':
