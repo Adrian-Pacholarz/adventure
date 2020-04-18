af93b88c72b267eac977b3bef838ccccea529634
lukasz lesiuk <lukasz.lesiuk.2@gmail.com>
2020-04-18 16:25:34 +0200

    addad combat

--- adventure-python/characters.py
+++ adventure-python/characters.py
@@ -2,10 +2,11 @@ characters = {
     "ghost": {
         "Character": "Ghost",
         "description": "A wild ghost appears!",
-        "items": f'This character is holding: {[chain]}',
+        # "items": f'This character is holding: {[chain]}',
         'max_hp': 15,
         'current_hp': 15,
         'base_attack': 3,
         'current_attack': 3,
-        'speed': 8
-    }
+        'base_speed': 8,
+        'current_speed': 8,}
+}
--- adventure-python/player_character.py
+++ adventure-python/player_character.py
@@ -10,23 +10,29 @@ player_character = {
     'current_hp': 100,
     'base_attack': 5,
     'current_attack': 5,
-    'speed': 3 
+    'base_speed': 3,
+    'current_speed': 3
 }
 
 
-def update_attack(player_status, inventory):
+def update_status(player_status, inventory):
     if 'sword' in inventory:
         player_character['current_attack'] = player_character['base_attack'] + 3
     elif 'dagger' in inventory:
         player_character['current_attack'] = player_character['base_attack'] + 1
 
+    if 'lama' in inventory:
+        player_character['current_speed'] = player_character['base_speed'] + 10
 
-def print_status(player_status):
-    pass
 
+    if player_status['current_hp'] > 100:
+        player_status['current_hp'] = 100
+    elif player_status['current_hp'] <= 0:
+        print("YOU DIED")
+        quit()
 
 
 if __name__ == '__main__':
     terminal_view.print_table(player_character)
-    update_attack(player_character, INVENTORY)
+    update_status(player_character, INVENTORY)
     terminal_view.print_table(player_character)
